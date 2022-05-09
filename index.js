99f26af30c87ba63795b0bdefee5d4d6a7602319
Travis Cook <cooktw@missouri.edu>
2022-05-09 09:33:24 -0400

    To tabs; Update packages

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const { join } = require( 'path' );
 
 module.exports = {
-  blockTemplatesPath: join( __dirname, 'templates/src' ),
-  pluginTemplatesPath: join( __dirname, 'templates/block' ),
-  defaultValues: {
-    author: 'Truman Tiger',
-    namespace: 'mizzou',
-    dashicon: 'carrot',
-  },
-  npmDependencies: [],
-  npmDevDependencies: [
-    {"@university-of-missouri/mizzou-design-system": "^3.0.0-beta.10"},
-    {"@university-of-missouri/browserslist-config": "^1.1.1"},
-  ]
+	blockTemplatesPath: join( __dirname, 'templates/src' ),
+	pluginTemplatesPath: join( __dirname, 'templates/block' ),
+	defaultValues: {
+		author: 'Truman Tiger',
+		namespace: 'mizzou',
+		dashicon: 'carrot',
+		description: 'A custom block.'
+	},
+	npmDependencies: [
+		'@university-of-missouri/mizzou-design-system'
+	],
+	npmDevDependencies: [
+		'@university-of-missouri/browserlist-config',
+		'classnames',
+	]
 }
