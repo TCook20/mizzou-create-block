a8322bcc6df29c1c09f54e4632e1b489313c1a7b
Travis Cook <cooktw@missouri.edu>
2022-05-10 11:36:16 -0400

    Switch to spaces; Rework to remove defaults

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const { join } = require( 'path' );
 
 module.exports = {
-	blockTemplatesPath: join( __dirname, 'templates/block' ),
-	pluginTemplatesPath: join( __dirname, 'templates/plugin' ),
-	defaultValues: {
-		author: 'Truman Tiger',
-		namespace: 'mizzou',
-		dashicon: 'carrot',
-		description: 'A custom block.',
-		npmDependencies: [
-			'@university-of-missouri/mizzou-design-system'
-		],
-		npmDevDependencies: [
-			'@university-of-missouri/browserslist-config',
-			'classnames',
-		]
-	},
+  blockTemplatesPath: join( __dirname, 'templates/block' ),
+  pluginTemplatesPath: join( __dirname, 'templates/plugin' ),
+  defaultValues: {
+    author: 'Truman Tiger',
+    namespace: 'mizzou',
+    dashicon: 'carrot',
+    description: 'A custom block.',
+    wpScripts: false,
+    wpEnv: false,
+    customScripts: {
+      'build': 'wp-scripts build',
+      'lint:css': 'wp-scripts lint-style',
+      'lint:js': 'wp-scripts lint-js',
+      'start': 'wp-scripts start'
+    },
+    npmDependencies: [
+      '@university-of-missouri/mizzou-design-system'
+    ],
+    npmDevDependencies: [
+      '@university-of-missouri/browserslist-config',
+      '@wordpress/block-editor',
+      '@wordpress/blocks',
+      '@wordpress/components',
+      '@wordpress/i18n',
+      '@wordpress/icons',
+      '@wordpress/scripts',
+      'classnames',
+    ]
+  },
 }
